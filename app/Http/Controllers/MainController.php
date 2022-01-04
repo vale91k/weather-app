103878ed3bf8c43adb877e452d148245f4755571
Kovalev.v <valenacheg_avantis@mail.ru>
2022-01-04 23:25:31 +0500

    Добавил контроллер для передачи ему данных, для вывода в шаблон

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+
+class MainController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @return Application|Factory|View
+     */
+    public function __invoke()
+    {
+        $data = [];
+
+        return view('home', $data);
+    }
+}
