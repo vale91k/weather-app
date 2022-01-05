be848c257559389f1cf4063b474d28264fc5c0dc
Kovalev.v <valenacheg_avantis@mail.ru>
2022-01-05 23:32:46 +0500

    Добавил миграцию и модель таблицы результатов прогноза погоды

---
+++ database/migrations/2022_01_05_100514_create_forecasts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForecastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forecasts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('city_id');
+            $table->date('date');
+            $table->decimal('temp', 5, 2);
+            $table->integer('clouds');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forecasts');
+    }
+}
